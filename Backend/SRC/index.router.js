57ed357f8978d795f9ebf136dd30641e42dc3fca
abeer-abubaker <abeerabubaker2001@gmail.com>
2025-07-03 20:26:12 +0300

    Connected project to MongoDB Atlas using Mongoose

--- Backend/SRC/index.router.js
+++ Backend/SRC/index.router.js
@@ -1,11 +1,9 @@
 import connectDB  from '../DB/connection.js';
 import cors from 'cors';
-import AuthRouter from "./modules/auth/auth.router.js";
 const initApp= async(app,express)=>{
    app.use(cors());
    connectDB();
     app.use(express.json());   
-    app.use('/auth',AuthRouter);
 
     app.use((err,req,res,next)=>{
     return  res.status(err.statusCode).json({ msg: err.message });
