37b2c8c206ef7cb4266c81139738e21ed906ea49
abeer-abubaker <abeerabubaker2001@gmail.com>
2025-07-13 22:23:52 +0300

    Add conditional rendering to Home page: show ChatContainer or NoChatSelected based on selected user

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import { useChatStore } from "../store/useChatStore";
+import ChatContainer from "../components/ChatContainer";
+import NoChatSelected from "../components/NoChatSelected";
 import Sidebar from "../components/Sidebar";
 
 const Home = () => {
+   const { selectedUser } = useChatStore();
   return (
     <div className="h-screen bg-purple-950 text-white">
       <div className="flex items-center justify-center pt-20 px-4">
@@ -8,11 +12,8 @@ const Home = () => {
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
 
-            <div className="flex-1 flex items-center justify-center">
-              <p className="text-purple-200 text-lg">
-                Select a contact to start chatting 💬
-              </p>
-            </div>
+  {!selectedUser ? <NoChatSelected /> : <ChatContainer/>}
+         
           </div>
         </div>
       </div>
